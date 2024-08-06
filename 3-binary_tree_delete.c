0cd6611e5bca85e5adaaf07ee102e6576b06fb4b
root <you@example.com>
2024-08-06 10:06:47 +0000

    betty descritption

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,15 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "binary_trees.h"
+/**
+ * binary_tree_delete - function for delete a tree
+ * @tree: pointers
+ */
 
 void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree == NULL)
+	{
 		return;
-
+	}
 		binary_tree_delete(tree->left);
 
 		binary_tree_delete(tree->right);
 
-		free (tree);
+		free(tree);
 }
--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,6 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "binary_trees.h"
+/**
+ * binary_tree_is_root - function for check if node is root
+ * @node: pointers
+ * Return: int
+ */
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
