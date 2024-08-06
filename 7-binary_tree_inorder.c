76d97a117fba4975ad59991b92a01950ec7a67c1
root <you@example.com>
2024-08-06 12:24:12 +0000

    binary tree

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+/**
+ * binary_tree_inorder - display a tree in order in a list
+ * @tree: pointers
+ * @func: pointers of function
+ */
+
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+	{
+		return;
+	}
+
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
+}
