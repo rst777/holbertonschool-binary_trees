da522c4fa6d1eb1e54d040c94d5968279c637e52
root <you@example.com>
2024-08-07 08:42:55 +0000

    correction

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,14 +9,13 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left_height = 0;
+	size_t right_height = 0;
 
 	if (tree == NULL)
 	{
 		return (0);
 	}
-
-	size_t left_height = 0;
-	size_t right_height = 0;
 
 	if (tree->left != NULL)
 	{
