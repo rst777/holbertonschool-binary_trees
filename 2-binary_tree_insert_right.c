4bf017e254f1eb1e235a939d8dfe6e9a0b20b6ab
Your Name <you@example.com>
2024-08-07 13:53:23 +0200

    correction

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -3,19 +3,19 @@
 #include <stdio.h>
 /**
  * binary_tree_insert_right - insert a node right on tree
- * @parent: pointers of tree parent
- * @value: pointers of value
- * Return: nomber of node
+ * @parent: pointers of parent node
+ * @value: value stored in the new node
+ * Return: Pointer to the created node, or NULL on failure
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node = binary_tree_node(parent, value);
+	binary_tree_t *new_node;
 
 	if (parent == NULL)
 	{
 		return (NULL);
 	}
-
+	new_node = binary_tree_node(parent, value);
 	if (new_node == NULL)
 	{
 		return (NULL);
