9b0ea660ce188b90f6e0e330a00e28d411e7ff39
root <you@example.com>
2024-08-07 09:19:09 +0000

    binary tree

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -13,20 +13,31 @@
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
+	size_t left_nodes = 0;
+	size_t right_nodes = 0;
+
 	if (tree == NULL)
 	{
 		return (0);
 	}
 
-	size_t left_height = binary_tree_nodes(tree->left);
-	size_t right_height = binary_tree_nodes(tree->right);
+
+	if (tree->left != NULL)
+	{
+		left_nodes = binary_tree_nodes(tree->left);
+	}
+
+	if (tree->right != NULL)
+	{
+		right_nodes = binary_tree_nodes(tree->right);
+	}
 
 	if (tree->left != NULL ||  tree->right != NULL)
 	{
-		return (1 + left_height + right_height);
+		return (1 + left_nodes + right_nodes);
 	}
 	else
 	{
-		return (left_height + right_height);
+		return (left_nodes + right_nodes);
 	}
 }
